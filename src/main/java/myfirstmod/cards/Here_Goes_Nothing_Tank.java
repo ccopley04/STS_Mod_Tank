70b5ccbe2ad9e274cacc69dc9d8b9152721c0584
Copley, Caleb Scott <ccopley7@gatech.edu>
2025-05-16 16:02:17 -0400

    Added Here Goes Nothing
    Added the common card "Here Goes Nothing". This card does considerable damage with downside of inflicting vulnerable onto the player.

---
+++ src/main/java/myfirstmod/cards/Here_Goes_Nothing_Tank.java
@@ -0,0 +1,46 @@
+package myfirstmod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.EntanglePower;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.powers.WeakPower;
+import com.megacrit.cardcrawl.vfx.combat.EntangleEffect;
+import myfirstmod.Tank.Tank;
+import myfirstmod.util.CardStats;
+
+public class Here_Goes_Nothing_Tank extends BaseCard {
+    public static final String ID = makeID("Here_Goes_Nothing");
+
+    private static final int DAMAGE = 15;
+    private static final int UPG_DAMAGE = 3;
+
+    private static final int VULNERABLE = 3;
+    private static final int UPG_VULNERABLE = -2;
+
+
+    private static final CardStats info = new CardStats(
+            Tank.Meta.DARK_GREEN_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
+            CardType.ATTACK, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
+            CardRarity.COMMON, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
+            CardTarget.ENEMY, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
+            1 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+    );
+
+    public Here_Goes_Nothing_Tank() {
+        super(ID, info); //Pass the required information to the BaseCard constructor.
+
+        setDamage(DAMAGE, UPG_DAMAGE); //Sets the card's damage and how much it changes when upgraded.
+        setMagic(VULNERABLE, UPG_VULNERABLE);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.SMASH));
+        addToBot(new ApplyPowerAction(p, p, new VulnerablePower(p, this.magicNumber, true)));
+    }
+}
