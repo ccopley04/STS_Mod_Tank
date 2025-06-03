4759b60e41fdd46e804abaa1ff1ed285e48fc14c
Copley, Caleb Scott <ccopley7@gatech.edu>
2025-06-03 17:10:35 -0400

    Added Obstruct
    Added the rare card Obstruct. This skill simply adds a great amount of block.

---
+++ src/main/java/myfirstmod/cards/Obstruct_Tank.java
@@ -0,0 +1,33 @@
+package myfirstmod.cards;
+
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import myfirstmod.Tank.Tank;
+import myfirstmod.util.CardStats;
+
+public class Obstruct_Tank extends BaseCard {
+    public static final String ID = makeID("Obstruct");
+
+    private static final int BLOCK = 20;
+    private static final int UPG_BLOCK = 10;
+
+    private static final CardStats info = new CardStats(
+            Tank.Meta.DARK_GREEN_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
+            CardType.SKILL, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
+            CardRarity.RARE, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
+            CardTarget.SELF, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
+            1 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+    );
+
+    public Obstruct_Tank() {
+        super(ID, info); //Pass the required information to the BaseCard constructor.
+
+        setBlock(BLOCK, UPG_BLOCK); //Sets the card's damage and how much it changes when upgraded.
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m){
+        addToBot(new GainBlockAction(p, block));
+    }
+}
