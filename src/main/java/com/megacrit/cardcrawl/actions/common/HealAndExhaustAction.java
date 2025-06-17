7faf5c0a8fa96eb552d269c453046bbd62068a8f
Copley, Caleb Scott <ccopley7@gatech.edu>
2025-06-17 17:56:20 -0400

    Added Scorched Earth
    Added the rare skill Scorched Earth. This card allows the player to pick any number of cards to exhaust and then heals 3 per card exhausted. It then exhausts itself. The card costs 2.

---
+++ src/main/java/com/megacrit/cardcrawl/actions/common/HealAndExhaustAction.java
@@ -0,0 +1,90 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package com.megacrit.cardcrawl.actions.common;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.UIStrings;
+
+public class HealAndExhaustAction extends AbstractGameAction {
+    private static final UIStrings uiStrings;
+    public static final String[] TEXT;
+    private AbstractPlayer p;
+    private boolean isRandom;
+    private boolean anyNumber;
+    private boolean canPickZero;
+    public static int numExhausted;
+    private int healAmount;
+
+    public HealAndExhaustAction(int amount, boolean isRandom, boolean anyNumber, boolean canPickZero, int healAmount) {
+        this.anyNumber = anyNumber;
+        this.p = AbstractDungeon.player;
+        this.canPickZero = canPickZero;
+        this.isRandom = isRandom;
+        this.amount = amount;
+        this.duration = this.startDuration = Settings.ACTION_DUR_FAST;
+        this.actionType = ActionType.EXHAUST;
+        this.healAmount = healAmount;
+    }
+
+    public void update() {
+        if (this.duration == this.startDuration) {
+            if (this.p.hand.size() == 0) {
+                this.isDone = true;
+                return;
+            }
+
+            if (!this.anyNumber && this.p.hand.size() <= this.amount) {
+                this.amount = this.p.hand.size();
+                numExhausted = this.amount;
+                int tmp = this.p.hand.size();
+
+                for(int i = 0; i < tmp; ++i) {
+                    AbstractCard c = this.p.hand.getTopCard();
+                    this.p.hand.moveToExhaustPile(c);
+                }
+
+                CardCrawlGame.dungeon.checkForPactAchievement();
+                return;
+            }
+
+            if (!this.isRandom) {
+                numExhausted = this.amount;
+                AbstractDungeon.handCardSelectScreen.open(TEXT[0], this.amount, this.anyNumber, this.canPickZero);
+                this.tickDuration();
+                return;
+            }
+
+            for(int i = 0; i < this.amount; ++i) {
+                this.p.hand.moveToExhaustPile(this.p.hand.getRandomCard(AbstractDungeon.cardRandomRng));
+            }
+
+            CardCrawlGame.dungeon.checkForPactAchievement();
+        }
+
+        if (!AbstractDungeon.handCardSelectScreen.wereCardsRetrieved) {
+            for(AbstractCard c : AbstractDungeon.handCardSelectScreen.selectedCards.group) {
+                p.heal(healAmount, false);
+                this.p.hand.moveToExhaustPile(c);
+            }
+
+            CardCrawlGame.dungeon.checkForPactAchievement();
+            AbstractDungeon.handCardSelectScreen.wereCardsRetrieved = true;
+        }
+
+        this.tickDuration();
+    }
+
+    static {
+        uiStrings = CardCrawlGame.languagePack.getUIString("ExhaustAction");
+        TEXT = uiStrings.TEXT;
+    }
+}
