8cc0b47422ebebaec51bc6fa7c217a98e24abfb0
Copley, Caleb Scott <ccopley7@gatech.edu>
2025-06-18 16:12:30 -0400

    Added Clean Sweep
    Added the uncommon card Clean Sweep. This card costs 2 and deals 12 damage to all enemies. Additionally, you discard your hand and draw that many cards.

---
+++ src/main/java/myfirstmod/cards/CleanSweep_Tank.java
@@ -0,0 +1,45 @@
+package myfirstmod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.*;
+import com.megacrit.cardcrawl.actions.unique.CalculatedGambleAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.vfx.combat.CleaveEffect;
+import myfirstmod.Tank.Tank;
+import myfirstmod.util.CardStats;
+
+public class CleanSweep_Tank extends BaseCard {
+    public static final String ID = makeID("Clean Sweep");
+
+    private static final int DAMAGE = 12;
+    private static final int UPG_DAMAGE = 0;
+
+
+    private static final CardStats info = new CardStats(
+            Tank.Meta.DARK_GREEN_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
+            CardType.ATTACK, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
+            CardRarity.UNCOMMON, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
+            CardTarget.ALL_ENEMY, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
+            2 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+    );
+
+    public CleanSweep_Tank() {
+        super(ID, info); //Pass the required information to the BaseCard constructor.
+
+        setDamage(DAMAGE, UPG_DAMAGE);
+        setCostUpgrade(1);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new SFXAction("ATTACK_HEAVY"));
+        this.addToBot(new VFXAction(p, new CleaveEffect(), 0.1F));
+        this.addToBot(new DamageAllEnemiesAction(p, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.NONE));
+        this.addToBot(new CalculatedGambleAction(false));
+    }
+}
